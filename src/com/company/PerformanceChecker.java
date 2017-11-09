ba66d0298a2ba6774bc17f654b82466ed530d187
Sasha Kaminska <sasha249133@ukr.net>
2017-11-10 00:52:05 +0200

    changed PerformanceChecker

--- src/com/company/PerformanceChecker.java
+++ src/com/company/PerformanceChecker.java
@@ -2,8 +2,6 @@ package com.company;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 public class PerformanceChecker {
 
@@ -12,27 +10,14 @@ public class PerformanceChecker {
         for (int i = 0; i < count; i++){
             collection.add(new Student());
         }
-        long afterFill = System.currentTimeMillis();
-        System.out.println("The total time is: "+ (afterFill - beforeFill) + "ms");
+        System.out.println("The total time is: "+ (System.currentTimeMillis() - beforeFill) + "ms");
     }
 
-    public static void checkReading(int count, List<Student> list){
+    public static void checkReading(int count, Collection<Student> collection){
         for (int i = 0; i < count; i++){
-            list.add(new Student());
+            collection.add(new Student());
         }
-        long timeMillis = System.currentTimeMillis();
-        for (int i = 0; i < count; i++) {
-            int index = (int) (Math.random() * count);
-            System.out.println(list.get(index));
-        }
-        System.out.println("The total time is: "+ (System.currentTimeMillis() - timeMillis) + "ms");
-    }
-
-    public static void checkReading(int count, Set<Student> set){
-        for (int i = 0; i < count; i++){
-            set.add(new Student());
-        }
-        Iterator<Student> iterator = set.iterator();
+        Iterator<Student> iterator = collection.iterator();
         Student student = new Student();
         long timeMillis = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
@@ -49,23 +34,12 @@ public class PerformanceChecker {
         System.out.println("The total time is: "+ (System.currentTimeMillis() - timeMillis) + "ms");
     }
 
-    public static void checkRemoving(int count, List<Student> list){
-        for (int i = 0; i < count; i++){
-            list.add(new Student());
-        }
-        long timeMillis = System.currentTimeMillis();
-        for (int i = 0; i < count; i++) {
-            int index = (int) (Math.random() * count);
-            list.remove(index);
-        }
-        System.out.println("The total time is: "+ (System.currentTimeMillis() - timeMillis) + "ms");
-    }
 
-    public static void checkRemoving(int count, Set<Student> set){
+    public static void checkRemoving(int count, Collection<Student> collection){
         for (int i = 0; i < count; i++){
-            set.add(new Student());
+            collection.add(new Student());
         }
-        Iterator<Student> iterator = set.iterator();
+        Iterator<Student> iterator = collection.iterator();
         Student student = new Student();
         long timeMillis = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
@@ -75,7 +49,7 @@ public class PerformanceChecker {
                     student  = iterator.next();
                 }
                 if (j == index){
-                   set.remove(student);
+                   collection.remove(student);
                 }
             }
         }
